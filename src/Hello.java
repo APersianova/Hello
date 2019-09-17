ec6efb585ccfc3c4b5888bfc9d862a5109afa506
APersianova <55406154+APersianova@users.noreply.github.com>
2019-09-17 13:41:30 +0300

    While

--- src/Hello.java
+++ src/Hello.java
@@ -2,16 +2,15 @@ import java.util.*;
 import java.math.*;
 
 
-public class Hello {
-    public static void main (String[] args){
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        if (n < 20) {
-            System.out.println(" меньше 20-ти");
-        }
-        else {
-            System.out.println("не меньше чем 20-ть");
-        }
+public class Hello{
+    static boolean condition() {
+       boolean result = Math.random() < 0.99;
+        System.out.println( result + ",");
+        return result;
     }
-
+public static void main (String[] args) {
+        while (condition())
+        System.out.println(" Inside 'While'");
+                    System.out.println("Exited 'While' ");
+}
 }
