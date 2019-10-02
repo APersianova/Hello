a47b74606dbdfa511cfaf9dbc614377788761c84
APersianova <55406154+APersianova@users.noreply.github.com>
2019-10-02 15:48:15 +0300

    Upper

---
+++ src/Upper.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Upper {
+        public static void main(String args[]){
+            Scanner sc = new Scanner(System.in);
+            String phrase = sc.nextLine();
+            String result = phrase.toUpperCase();
+            System.out.println("Result: " + result);
+        }
+    }
+
