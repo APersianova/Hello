7d4852957c16fb27568f8a59190aaec77e9e0b85
APersianova <55406154+APersianova@users.noreply.github.com>
2019-10-02 15:59:24 +0300

    Update Upper.java

--- src/Upper.java
+++ src/Upper.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Upper {
         public static void main(String args[]){
+            System.out.println("Enter a world or phase:");
             Scanner sc = new Scanner(System.in);
             String phrase = sc.nextLine();
             String result = phrase.toUpperCase();
